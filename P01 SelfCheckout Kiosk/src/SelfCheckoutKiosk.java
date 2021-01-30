56c4e403b2bd0200f8fa4fa024e54c86c22f260e
Agastya-Asthana <agastya.asthana@gmail.com>
2021-01-29 21:59:16 -0600

    Project init

---
+++ P01 SelfCheckout Kiosk/src/SelfCheckoutKiosk.java
@@ -0,0 +1,31 @@
+//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
+//
+// Title:    A self checkout kiosk program
+// Course:   CS 300 Fall 2020
+//
+// Author:   Agastya Asthana
+// Email:    aasthana3@wisc.edu
+// Lecturer: Mouna Kacem
+//
+///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
+//
+// Persons:         (identify each by name and describe how they helped)
+// Online Sources:  github.com: helped with source control
+//
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This class handles the kiosk interaction
+ * 
+ */
+public class SelfCheckoutKiosk {
+    public static final double TAX_RATE = 0.06; // Sales Tax
+
+    public static final String[][] GROCERY_ITEMS = new String[][] { { "Apple", "$1.59" }, { "Avocado", "$0.59" },
+            { "Banana", "$0.49" }, { "Beef", "$3.79" }, { "Blueberry", "$6.89" }, { "Broccoli", "$1.79" },
+            { "Butter", "$4.59" }, { "Carrot", "$1.19" }, { "Cereal", "$3.69" }, { "Cheese", "$3.49" },
+            { "Chicken", "$5.09" }, { "Chocolate", "$3.19" }, { "Cookie", "$9.5" }, { "Cucumber", "$0.79" },
+            { "Eggs", "$3.09" }, { "Grape", "$2.29" }, { "Ice Cream", "$5.39" }, { "Milk", "$2.09" },
+            { "Mushroom", "$1.79" }, { "Onion", "$0.79" }, { "Pepper", "$1.99" }, { "Pizza", "$11.5" },
+            { "Potato", "$0.69" }, { "Spinach", "$3.09" }, { "Tomato", "$1.79" } };
+}
