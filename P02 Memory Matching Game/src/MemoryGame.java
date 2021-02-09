fceb758b0f74b967fb4441b52bac80ce1714b8c3
Agastya-Asthana <agastya.asthana@gmail.com>
2021-02-09 12:15:00 -0600

    Nearly finished project

--- P02 Memory Matching Game/src/MemoryGame.java
+++ P02 Memory Matching Game/src/MemoryGame.java
@@ -25,9 +25,11 @@ public class MemoryGame{
   private static Card selectedCard2; // Second selected card
   private static boolean winner; // boolean evaluated true if the game is won,
   // and false otherwise
-  private static int matchedCardsCount; // number of cards matched so far
+  private static int matchedCardsCount = 0; // number of cards matched so far
   // in one session of the game
   private static String message; // Displayed message to the display window
+  // number of cards selected
+  private static int numOfCardsSelected = 0;
 
   /**
   * Defines the initial environment properties of this game as the program starts
@@ -49,6 +51,7 @@ public class MemoryGame{
     selectedCard1 = null;
     selectedCard2 = null;
     matchedCardsCount = 0;
+    numOfCardsSelected = 0;
     winner = false;
     message = "";
 
@@ -71,8 +74,34 @@ public class MemoryGame{
   */
   public static void draw(){
     // Set the color used for this background of the Processing window
-    processing.background(32, 48, 56); // Grayish black
+    processing.background(232, 228, 201); // dirty white
     for (int i = 0; i < cards.length; i++) cards[i].draw();
+
+    if (selectedCard1 != null && selectedCard2 != null) {
+      if (matchingCards(selectedCard1, selectedCard2) && numOfCardsSelected < 3){
+        displayMessage("CARDS MATCHED! Good Job!");
+        selectedCard1.setMatched(true);
+        selectedCard2.setMatched(true);
+        matchedCardsCount++;
+      }
+      else if (!matchingCards(selectedCard1, selectedCard2) && numOfCardsSelected < 3){
+        displayMessage(NOT_MATCHED);
+      }
+      else if (numOfCardsSelected == 3) {
+        numOfCardsSelected = 0;
+        selectedCard1 = null;
+        selectedCard2 = null;
+        for (int i = 0; i < cards.length; i++) {
+          if (!cards[i].isMatched() && !isMouseOver(cards[i])) {
+            cards[i].deselect(); cards[i].setVisible(false);
+          }
+          if (isMouseOver(cards[i])) {
+            selectedCard1 = cards[i];
+            numOfCardsSelected++;
+          }
+        }
+      }
+    }
   }
 
   /**
@@ -92,9 +121,58 @@ public class MemoryGame{
   * @return true if the mouse is over the storage list, false otherwise
   */
   public static boolean isMouseOver(Card card){
-    PImage cardImage = card.getImage();
+    boolean inRangeX = false, inRangeY = false;
+    float imageX = card.getX(), imageY = card.getY(); 
+    int imageHeight = card.getHeight(), imageWidth = card.getWidth();
+
+    inRangeX = (Math.abs(processing.mouseX-imageX) <= imageWidth/2) ? true : false;
+    inRangeY = (Math.abs(processing.mouseY - imageY) <= imageHeight / 2) ? true : false;
+
+    return (inRangeX && inRangeY) ? true: false;
     
   }
+
+  /**
+  * Callback method called each time the user presses the mouse
+  */
+  public static void mousePressed(){
+
+    for (int i = 0; i < cards.length; i++) {
+      if (isMouseOver(cards[i]) && !cards[i].isMatched()) {
+        numOfCardsSelected++;
+        if (selectedCard1 == null) {
+          selectedCard1 = cards[i];
+          selectedCard1.setVisible(true);
+          selectedCard1.select();
+          break;
+        }
+        else if (selectedCard1 != null){ 
+          selectedCard2 = cards[i];
+          if (selectedCard1 == selectedCard2) {
+            selectedCard2 = null;
+          }
+          else{
+            selectedCard2.setVisible(true);
+            selectedCard2.select();
+            break;
+          }
+          
+        }
+      }
+    }
+
+  }
+
+  /**
+  * Checks whether two cards match or not
+  * @param card1 reference to the first card
+  * @param card2 reference to the second card
+  * @return true if card1 and card2 image references are the same, false otherwise
+  */
+  public static boolean matchingCards(Card card1, Card card2){
+    return (card1.getImage() == card2.getImage()) ? true : false;
+  }
+
   public static void main(String[] args) {
     Utility.startApplication();
   }
